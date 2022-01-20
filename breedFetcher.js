dccb414d1d84cc6648f974f78d7a94724688dcd0
Adam Schulte <adamschulte148@hotmail.com>
2022-01-20 20:47:39 +0000

    refactoring code

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,26 +4,33 @@ const fs = require("fs");
 // API url:
 // https://api.thecatapi.com/v1/breeds/search
 
-const input = process.argv.slice(2);
+const url = "https://api.thecatapi.con/v1/breeds/search/?q=";
+// const input = process.argv.slice(2);
 
-const fetcher = (url, catBreed) => {
+const fetchBreedDescription = function(catBreed, callback) {
   request(
     url + catBreed, //website user requests
     (error, response, body) => {
       if (error) {
-        console.log("ERROR:", error);
+        // console.log("ERROR:", error);
+        callback(error, null)
       }
-
       //convert to an object
       const data = JSON.parse(body);
       const breed = data[0];
       if (breed) {
-        console.log(breed.description);
-        return breed.description;
-      }
-      console.log(`Breed ${catBreed} not found`);
+        callback(null, breed.description);
+        // return breed.description;
+        } 
+        callback(null, `Breed not found:`)          
     }
-  );
+  );  
 };
 
-fetcher("https://api.thecatapi.com/v1/breeds/search/?q=", input[0]);
+module.exports = { fetchBreedDescription };
+
+// const fetcher = (url, catBreed) => {
+
+// };
+
+// fetcher("https://api.thecatapi.com/v1/breeds/search/?q=", input[0]);
